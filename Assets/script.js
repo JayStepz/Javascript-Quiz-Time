a9dab0794b5188281ccb7dd4bfc04db07c5a0415
JayStepz <jakob_s_stephens@hotmail.com>
2023-02-15 20:37:14 -0500

    Added classes in HTML, added h1 h2 lines, and nested some divs. Got style.css to a somewhat useable point. Working on Javascript next.

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+var win = document.querySelector("win");
+var lose = document.querySelector("loss");
+var timerElement = document.querySelector("timer");
+var startButton = document.querySelector("begin-button");
+var quiz = document.querySelector("quiz-card");
+
+var winCounter = 0;
+var loseCounter = 0;
+var timer;
+var timerTime;
