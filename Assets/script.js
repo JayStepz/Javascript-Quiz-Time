e9ee6f60869a5ce0705d6605036c86610a05ed9a
JayStepz <jakob_s_stephens@hotmail.com>
2023-02-19 20:34:06 -0500

    Changed some names, changed score card to view score button area, erased a lot of js to start anew.

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,17 @@
-var win = document.querySelector(".win");
-var lose = document.querySelector(".loss");
-var timerElement = document.querySelector(".timer");
-var beginButton = document.querySelector(".begin-button");
-var resetButton = document.querySelector(".reset-button");
-var quiz = document.querySelector(".quiz-card");
+var correct = document.querySelector("correct");
+var lose = document.querySelector("incorrect");
+var timerElement = document.querySelector("timer");
+var beginButton = document.querySelector("begin-button");
+var resetButton = document.querySelector("reset-button");
+var viewButton = document.querySelector("view-scores");
+var quiz = document.querySelector("quiz-card");
 
-var winCounter = 0;
-var loseCounter = 0;
 var timer;
-var timerTime;
-var didWin = false;
+var timerCount;
 var corrAns;
 
-var QAs = [QA1, QA2, QA3, QA4, QA5];
-
-const QA1 = {
+/* var QAs = [
+    {
     question: "Is JavaScript case-sensitive?",
     answers: {
     a: "Yes",
@@ -23,9 +20,9 @@ const QA1 = {
     d: "Only functions"
     },
     correctAnswer: "a"
-}
+},
 
-const QA2 = {
+{
     question:"How do you add a comment in JavaScript?",
     answers: {
     a:"<!--Comment Here-->",
@@ -34,9 +31,9 @@ const QA2 = {
     d:"(Comment Here)",
     },
     correctAnswer: "b"
-}
+},
 
-const QA3 = {
+ {
     question:"Which one of these is not a data type?",
     answers: {
     a:"String",
@@ -45,9 +42,9 @@ const QA3 = {
     d:"Boolean",
     },
     correctAnswer: "c"
-}
+},
 
-const QA4 = {
+{
     question:"Arrays in Javascript can be used to store:",
     answers: {
     a:"Strings and Numbers",
@@ -56,9 +53,9 @@ const QA4 = {
     d:"All of the above",
     },
     correctAnswer: "d"
-}
+},
 
-const QA5 = {
+{
     question:"An if/then statement must be wrapped in one of these.",
     answers: {
     a:"()",
@@ -68,84 +65,40 @@ const QA5 = {
     },
     correctAnswer: "b"
 }
+]*/
 
 /*function displayQA {
-    chosenQA = QAs[Math.floor(Math.random() * QAs.length)];
+    var chosenQA = QAs[Math.floor(Math.random() * QAs.length)];
 
     for (var i = 0; i < QAs.length; i++) {
 
     }
 }*/
 
-/*function scoreTable {
-
-}*/
-
-function init() {
-    getWins();
-    getLosses();
-}
-
-function beginGame() {
-    didWin = false;
+function beginQuiz() {
     timerTime = 60;
     timerStart()
     displayQA()
 }
 
-function getWins() {
-    var storWin = localStorage.getItem("win");
-
-    if (storWin === null) {
-        winCounter = 0;
-    }   else {
-        winCounter = storWin;
-    }
-    // Win message
-}
-
-function win() {
-    if (corrAns === 5) {
-    winCounter + 1;
-    }
-}
-
-function getLosses() {
-    var storWin = localStorage.getItem("loss");
-
-    if (storLoss === null) {
-        winCounter = 0;
-    }   else {
-        loseCounter = storWin;
-    }
-    // Lose message
-}
-
-function lose() {
-    if (corrAns < 5) {
-    loseCounter = 0;
-    }
-}
-
 function timerStart() {
     timer = setInterval(function() {
-        timerTime - 1;
-        timerElement.textContent = timerTime;
-        if (timerTime >= 0) {
-            if (didWin && timerTime >0) {
+        timerCount --;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            if (timerCount >0) {
             // Log time as score
-            win();
-            
             }
         }
 
-        if (timerTime === 0) {
+        if (timerCount === 0) {
             clearInterval(timer);
             // End game
         }
     }, 1000);
 }
 
-beginButton.addEventListener("click", beginGame);
-
 init();
+
+beginButton.addEventListener("click", beginGame);
+resetButton.addEventListener("click", )
